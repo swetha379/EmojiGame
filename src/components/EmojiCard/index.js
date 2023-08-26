f39df21747e2ffade9032c77cac76b8b2893b75c
swetha379 <swethaajmera3@gmail.com>
2023-08-26 14:40:59 +0000

    Emoji game page

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,19 @@
+// Write your code here.
+import './index.css'
+
+const EmojiCard = props => {
+  const {emojiDetails, clickEmoji} = props
+  const {id, emojiName, emojiUrl} = emojiDetails
+
+  const onClickEmojiCard = () => {
+    clickEmoji(id)
+  }
+  return (
+    <li className="emoji-item">
+      <button className="emoji-btn" type="button" onClick={onClickEmojiCard}>
+        <img className="emoji-icon" src={emojiUrl} alt={emojiName} />
+      </button>
+    </li>
+  )
+}
+export default EmojiCard
